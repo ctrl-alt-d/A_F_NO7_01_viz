2a3e74c1e8188f7e166c387a604fa7f7ba3db7e0
dani herrera <ctrl.alt.d@gmail.com>
2019-02-24 20:04:25 +0100

    WIP. Deserialization track issues with Tonejs/Midi (has time and duration)

--- src/sketch.js
+++ src/sketch.js
@@ -1,86 +1,113 @@
 import p5 from "p5"
 import 'p5/lib/addons/p5.sound';
-import parse from 'url-parse';
-import request from 'request';
-import { parseArrayBuffer } from 'midi-json-parser';
-import { loadFixtureAsArrayBuffer, loadFixtureAsJson } from './helper'
+import Midi  from '@tonejs/midi';
+import Tone  from 'tone';
 
 const scores = require('../scores/*.mid');
 
 import webmidi from 'webmidi'
+import { timeout } from "q";
 
 var viz_function = function(p) {
 
-    p.monoSynth = new p5.MonoSynth();
+  p.monoSynth = new p5.MonoSynth();
 
-     
-    let url = parse(window.location.href , true);
+  p.MidiJson = null;
+  p.AllNotes = [];
+  p.MillisBase = null;
+  p.PreviousMillis = 0;
 
-    p.playJsonMidi = (jsonMidi) => {
-      console.log('jsonMidi.tracks[0] :', jsonMidi.tracks[0].filter(x=> x.noteOn != null || (x.noteOff != null && x.noteOff.velocity > 0 )));
-      //p.monoSynth.play(e.note.name + e.note.octave, e.velocity, 0, 1/6);
+  p.Synths = [];
+
+  p.setup = function() {
+
+    p.frameRate(60);
+    p.noLoop();
+    var myCanvas = p.createCanvas(800,600);
+    myCanvas.parent('viz2');
+    p.noStroke();
+
+    //webmidi
+    webmidi.enable(function (err) {
+      webmidi.inputs.forEach( input =>  {
+                              input.addListener('noteon', "all",e=>p.noteon(e) )
+                              input.addListener('noteoff', "all",e=>p.noteoff(e) )          
+                                        }
+                            );
+    });
+
+    p.CreaSynths(20);
+    Midi.fromUrl(scores["TheEnterntainer"])
+        .then( p.playMidiFile );
+
+  }
+
+  // - Draw -------------------------------------------
+
+  p.draw = () => {
+    if (p.MidiJson == null ) return;
+    let currentMillis = p.millis() - p.MillisBase;
+    let notesToDraw = p.AllNotes.filter(n => n.time*1000 >= p.PreviousMillis && n.time*1000 < currentMillis );
+    if (notesToDraw.length>0) console.log('notes :', notesToDraw.map(note=>note.name).join(",") );
+    notesToDraw.forEach(note => p.addSynth( note ) );
+    p.PreviousMillis = currentMillis;
+  }
+
+  p.addSynth = (note) => {
+    let syntFreeElements = p.Synths.filter(s=>s.isfree) || [];
+    if (syntFreeElements.length > 0) {
+      let syntElement = syntFreeElements[0];
+      syntElement.isfree = false;
+      syntElement.synth.triggerAttackRelease(note.name, note.duration, Tone.now() + 0.5, note.velocity);
+      setTimeout( ()=> p.rmSynth(syntElement) , note.duration*1000+10  );
     }
-
-    p.carregaMidi = (rr, arrayBuffer) => {
-      parseArrayBuffer(arrayBuffer)
-      .then( p.playJsonMidi )
-      .catch( (e) => {console.log('e :', e);} )
+    else {
+      console.warn("M'he quedat sense sintetitzadors");
     }
+  }
 
-    loadFixtureAsArrayBuffer(scores["A_F_NO7_01"], p.carregaMidi );
+  p.rmSynth = (syntElement) => {
+    syntElement.isfree = true; 
+  }
 
-    p.setup = function() {
-      var myCanvas = p.createCanvas(800,600);
-      myCanvas.parent('viz2');
-      p.noStroke();
-      p.noLoop();
+  // - Midi file ----------------------------------------
 
-      //webmidi
-      webmidi.enable(function (err) {
-        webmidi.inputs.forEach( input =>  {
-                                input.addListener('noteon', "all",e=>p.noteon(e) )
-                                input.addListener('noteoff', "all",e=>p.noteoff(e) )          
-                                          }
-                              );
-      });
+  p.playMidiFile = (midiJson) => {
+    p.MidiJson = midiJson;
+    console.log('p.MidiJson :', p.MidiJson);
+    p.MidiJson
+      .tracks
+      .forEach(track => {p.AllNotes.push( ...track.notes ); console.log('p.AllNotes.len :', p.AllNotes.length);} );
+    p.MillisBase = p.millis();
+    p.loop();
+  }
 
-      //test sound
-      myCanvas.mousePressed(this.playSynth);
+  // - Synth
 
+  p.CreaSynths = (n) => {
+    console.log('Creant sintetitzadors :');
+    for (let i = 0; i< n; i ++ ) {
+      let synth = new Tone.PolySynth(n, Tone.Synth, {
+        envelope : {
+          attack : 0.02,
+          decay : 0.1,
+          sustain : 0.3,
+          release : 1
+        }
+      }).toMaster();
+      p.Synths[i] = { id:i, isfree: true, synth: synth};
+    }      
+    console.log('Sintetitzadors creats :');
+  }
 
-    }
-  
-    p.draw = () => {
-      p.background(255,255,255)
-      p.fill(0,255,0)
-      p.ellipse(p.mouseX,p.mouseY,20,20)
-    }
-  
-    p.mouseMoved = () => {
-        p.redraw()
-    }
+  // - MIDI Events --------------------------------------------
 
-    p.noteon = (e) => {
-      p.monoSynth.play(e.note.name + e.note.octave, e.velocity, 0, 1/6);
-    }
-    p.noteoff = (e) => {
-    }
-    
-    p.playSynth = () => {
-      // time from now (in seconds)
-      var time = 0;
-      // note duration (in seconds)
-      var dur = 1/6;
-      // note velocity (volume, from 0 to 1)
-      var v = p.random();
-    
-      p.monoSynth.play("Fb5", v, 0, dur);
-      p.monoSynth.play("Gb5", v, time += dur, dur);
-    
-      p.background( p.random(255), p.random(255), 255);
-      p.text('click to play', p.width/2, p.height/2);
-    }
-    
+  p.noteon = (e) => {
+    p.monoSynth.play(e.note.name + e.note.octave, e.velocity, 0, 1/6);
+  }
+  p.noteoff = (e) => {
   }
   
-  var viz = new p5(viz_function)
+}
+
+var viz = new p5(viz_function)
